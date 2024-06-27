67e696efa5d63cc3d07995e13c8476f1b5072136
Bryan Odiña <149803435+RedCheeksCoder@users.noreply.github.com>
2024-06-27 14:54:59 +0800

    Add files via upload

---
+++ Digital-Business-Card-main/src/App.jsx
@@ -0,0 +1,178 @@
+import "./App.css";
+import BusinessCard from "./BusinessCard";
+import charlie from "./images/charlie.jpg";
+import evalyn from "./images/evalyn.jpg";
+import james from "./images/james.jpg";
+import jlou from "./images/jlou.jpg";
+import joan from "./images/joan.jpg";
+import joward from "./images/joward.jpg";
+import rez from "./images/rez.jpg";
+import ruth from "./images/ruth.jpg";
+import sarah from "./images/sarah.jpg";
+import rolli from "./images/rolli.jpg";
+import analou from "./images/analou.jpg";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useParams,
+} from "react-router-dom";
+
+const employees = [
+  {
+    id: 1,
+    name: "Belvis, Charlie D.",
+    firstName: "Charlie",
+    middleName: "D.",
+    lastName: "Belvis",
+    contact: "+639088636156",
+    position: "Sales Engineer",
+    email: "charlie.belvis@inzpect.com",
+    displayPic: charlie,
+  },
+  {
+    id: 2,
+    name: "Bendijo, Joward V.",
+    firstName: "Joward",
+    middleName: "V.",
+    lastName: "Bendijo",
+    contact: "+639088923623",
+    position: "Sales Engineer",
+    email: "joward.bendijo@inzpect.com",
+    displayPic: joward,
+  },
+  {
+    id: 3,
+    name: "Cariño, Joana Jane M.",
+    firstName: "Joana Jane",
+    middleName: "M.",
+    lastName: "Cariño",
+    contact: "+6399088923621",
+    position: "Technical Sales",
+    email: "joana.carino@inzpect.com",
+    displayPic: joan,
+  },
+  {
+    id: 4,
+    name: "Dela Cruz, Renz D.",
+    firstName: "Renz",
+    middleName: "D.",
+    lastName: "Dela Cruz",
+    contact: "+639088923622",
+    position: "Technical Sales",
+    email: "renz.delacruz@inzpect.com",
+    displayPic: rez,
+  },
+  {
+    id: 5,
+    name: "Francisco, Sarah Jane L.",
+    firstName: "Sarah Jane",
+    middleName: "L.",
+    lastName: "Francisco",
+    contact: "+639088959060",
+    position: "Technical Sales",
+    email: "sarah.jane.francisco@inzpect.com",
+    displayPic: sarah,
+  },
+  {
+    id: 6,
+    name: "Laya, Rolando T.",
+    firstName: "Rolando",
+    middleName: "T.",
+    lastName: "Laya",
+    contact: "+639189215744",
+    position: "President / Sales & Marketing Manager",
+    email: "rolando_laya@inzpect.com",
+    displayPic: rolli,
+  },
+  {
+    id: 7,
+    name: "Laurenio, Jm Ruth Y.",
+    firstName: "Jm Ruth",
+    middleName: "Y.",
+    lastName: "Laurenio",
+    contact: "+639499946485",
+    position: "Technical Sales",
+    email: "ruth.laurenio@inzpect.com",
+    displayPic: ruth,
+  },
+  {
+    id: 8,
+    name: "Paculba, Ma. Evalyn M.",
+    firstName: "Ma. Evalyn",
+    middleName: "M.",
+    lastName: "Paculba",
+    contact: "+639499946487",
+    position: "Technical Sales",
+    email: "evalyn.paculba@inzpect.com",
+    displayPic: evalyn,
+  },
+  {
+    id: 9,
+    name: "Ruiz, Joanah Lou I.",
+    firstName: "Joanah Lou",
+    middleName: "I.",
+    lastName: "Ruiz",
+    contact: "+639088959061",
+    position: "Technical Sales",
+    email: "joanah.ruiz@inzpect.com",
+    displayPic: jlou,
+  },
+  {
+    id: 10,
+    name: "Santos, James Roy B.",
+    firstName: "James Roy",
+    middleName: "B.",
+    lastName: "Santos",
+    contact: "+639088923618",
+    position: "Senior Sales Supervisor",
+    email: "james.santos@inzpect.com",
+    displayPic: james,
+  },
+  {
+    id: 11,
+    name: "Tunan, Maria Analou R.",
+    firstName: "Maria Analou",
+    middleName: "R.",
+    lastName: "Tunan",
+    contact: "+639499946486",
+    position: "Sales Coordinator",
+    email: "maria.analou.tunan@inzpect.com",
+    displayPic: analou,
+  },
+   {
+    id: 12,
+    name: "Gianan, Jon Alvin G.",
+    firstName: "Jon Alvin",
+    middleName: "G.",
+    lastName: "Gianan",
+    contact: "+639088959061",
+    position: "Technical Sales",
+    email: "jon.gianan@inzpect.com",
+    displayPic: rolli,
+  },
+];
+
+const imageData = [{ airplane: "./account.png" }];
+
+const EmployeeCardWrapper = () => {
+  const { id } = useParams();
+  const employee = employees.find((emp) => emp.id === parseInt(id));
+  console.log("employee");
+  return <BusinessCard employee={employee} imageData={imageData} />;
+};
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="employee/:id" element={<EmployeeCardWrapper />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ Digital-Business-Card-main/src/BusinessCard.jsx
@@ -0,0 +1,197 @@
+import styled from "styled-components";
+import website from "./images/website.png";
+import briefcase from "./images/briefcase.png";
+import facebook from "./images/facebook.png";
+import mail from "./images/mail.png";
+import phone2 from "./images/phone2.png";
+import vCard from "vcards-js";
+
+//=====================VCF====================
+const Container = styled.div`
+  max-width: 420px;
+  min-width: 300px;
+  margin: auto;
+  padding: 1rem;
+`;
+const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-family: " Inter ", sans-serif;
+  box-sizing: border-box;
+  background-color: rgb(251, 221, 0);
+  border: 2px solid #f8f4f4;
+  img {
+    height: 100px;
+    width: 100px;
+    border-radius: 50%;
+  }
+`;
+const Body = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  background-color: #f8f4f4;
+  border: 2px solid #8f8f8f;
+`;
+const Footer = styled.div``;
+const ContactIcons = styled.div``;
+const AddToContact = styled.a`
+  font-family: " Inter ", sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #0b465b;
+  color: white;
+  font-size: 15px;
+  font-weight: 500;
+  width: 150px;
+  height: 50px;
+  margin-bottom: 1rem;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+`;
+const Name = styled.h1`
+  font-size: 1.5rem;
+  font-weight: 400;
+`;
+const Title = styled.h5`
+  font-size: 0.8rem;
+  font-weight: 300;
+  padding: 0.3rem 0 1.5rem 0;
+  color: gray;
+`;
+
+const StyledImage = styled.img`
+  margin: 2rem 0 1rem 0;
+`;
+
+const Contact = styled.div`
+  font-family: " Inter ", sans-serif;
+  background-color: #f8f4f4;
+  font-size: 0.4rem;
+  display: flex;
+  align-items: center;
+  padding: 0.2rem;
+  margin: 1rem;
+  h4 {
+    color: #808080;
+  }
+  border-bottom: 10px;
+`;
+
+const Info = styled.p`
+  a {
+    font-size: large;
+    word-wrap: break-word;
+    overflow-wrap: break-word;
+    white-space: normal;
+  }
+  p {
+    font-size: large;
+    word-wrap: break-word;
+    overflow-wrap: break-word;
+    white-space: normal;
+  }
+  h4 {
+    font-size: 0.8rem;
+  }
+`;
+const Icon = styled.img`
+  padding-right: 1rem;
+`;
+const BusinessCard = ({ employee }) => {
+  if (!employee) return <div>Employee not found</div>;
+
+  const vcf = vCard();
+  vcf.firstName = employee.firstName;
+  vcf.middleName = employee.middleName;
+  vcf.lastName = employee.lastName;
+  vcf.cellphone = employee.contact;
+  vcf.organization = employee.company;
+  vcf.email = employee.email;
+  vcf.role = employee.position;
+
+  //   vcf.saveToFile(`./${employee.firstName}.vcf`);
+
+  //console.log(vcf.getFormattedString());
+  const fileName = employee.firstName.toLowerCase().replace(/\s+/g, "");
+  console.log(fileName);
+  return (
+    <>
+      <Container>
+        <Header>
+          <StyledImage
+            src={employee.displayPic}
+            alt={employee.name}
+            height={150}
+            width={150}
+          />
+          <Name>{employee.name}</Name>
+          <Title>{employee.position}</Title>
+
+          <AddToContact href={`/${fileName}.vcf`} download>
+            <p>Add to Contacts</p>
+          </AddToContact>
+          <ContactIcons></ContactIcons>
+        </Header>
+        <Body>
+          <Contact>
+            <Icon src={phone2} alt={employee.contact} height={30} width={30} />
+            <Info>
+              <p>{employee.contact}</p>
+              <h4>Mobile</h4>
+            </Info>
+          </Contact>
+          <Contact>
+            <Icon src={mail} alt={employee.email} height={30} width={30} />
+            <Info>
+              <p>{employee.email}</p>
+              <h4>Email</h4>
+            </Info>
+          </Contact>
+          <Contact>
+            <Icon
+              src={briefcase}
+              alt="Description of image"
+              height={30}
+              width={30}
+            />
+            <Info>
+              <p>Inzpect Technologies, Inc.</p>
+              <h4>Company</h4>
+            </Info>
+          </Contact>
+          <Contact>
+            <Icon
+              src={website}
+              alt="Description of image"
+              height={30}
+              width={30}
+            />
+            <Info>
+              <a href="https://web.inzpect.com/">Inzpect Website</a>
+              <h4>Website</h4>
+            </Info>
+          </Contact>
+          <Contact>
+            <Icon
+              src={facebook}
+              alt="Description of image"
+              height={30}
+              width={30}
+            />
+            <Info>
+              <a href="https://www.facebook.com/inzpectech">Inzpect Facebook Page</a>
+              <h4>Facebook page</h4>
+            </Info>
+          </Contact>
+        </Body>
+        <Footer></Footer>
+      </Container>
+    </>
+  );
+};
+export default BusinessCard;
